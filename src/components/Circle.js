af3381847542662f3327dbe3fc0558aeb0e5ccfb
Morgan Fudge <mfudge92@gmail.com>
2018-05-10 18:11:47 +0000

    automatically backed up by learn

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -11,7 +11,6 @@ class Circle extends React.Component {
 
   render() {
     const { color } = this.props;
-    console.log(`A ${color} circle was rendered!`);
     return (
       <div className={`circle ${color}`}></div>
     )
